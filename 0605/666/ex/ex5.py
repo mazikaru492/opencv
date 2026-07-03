509c351d41942645408c66d92c4c49d42ff7536c
yuki <ktc25a31e0014@edu.kyoto-tech.ac.jp>
2026-07-03 10:13:17 +0900

    0703

---
+++ 0605/666/ex/ex5.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def main():
+    a = np.array([[1,2,3,4,5]])
+    print(a + 10)
+
+if __name__ == "__main__":
+    main()
---
+++ 0605/666/ex/toi3.py
@@ -0,0 +1,12 @@
+import numpy as np
+def main():
+    a = np.array([[1,2,3,4,5]])
+    print(a * 2)
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
